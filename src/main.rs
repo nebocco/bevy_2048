bd05d9f812859e6651c1aa537e49597fbdc4ee71
nebocco <nebocco@gmail.com>
2022-01-27 19:05:49 +0900

    add: tile move system

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,39 @@ struct Position {
 }
 
 impl Position {
-    fn new(x: i32, y: i32) -> Self {
-        Self { x, y }
+    fn index(&self) -> usize {
+        self.y as usize * SIDE_LENGTH + self.x as usize
+    }
+
+    fn from_index(i: usize) -> Self {
+        Self {
+            x: (i % SIDE_LENGTH) as i32,
+            y: (i / SIDE_LENGTH) as i32,
+        }
     }
 }
 
 impl From<Position> for Transform {
     fn from(pos: Position) -> Self {
-        let x = (pos.x as f32 - (SIDE_LENGTH - 1) as f32 / 2.) * (TILE_SIZE * 1.05);
-        let y = (pos.y as f32 - (SIDE_LENGTH - 1) as f32 / 2.) * (TILE_SIZE * 1.05);
+        let x = (pos.x as f32 - 1.5) * (TILE_SIZE * 1.05);
+        let y = (pos.y as f32 - 1.5) * (TILE_SIZE * 1.05);
         Transform::from_xyz(x, y, 0.)
     }
 }
 
 #[derive(Component)]
 struct Tile(u64);
+
+impl From<Tile> for Color {
+    fn from(Tile(num): Tile) -> Self {
+        match num {
+            2 => Color::GOLD,
+            4 => Color::ORANGE,
+            8 => Color::ORANGE_RED,
+            _ => Color::RED,
+        }
+    }
+}
 
 fn create_tile(commands: &mut Commands, num: u64, position: Position) {
     commands
@@ -34,7 +52,7 @@ fn create_tile(commands: &mut Commands, num: u64, position: Position) {
         .insert(position.clone())
         .insert_bundle(SpriteBundle {
             sprite: Sprite {
-                color: Color::ORANGE,
+                color: Color::from(Tile(num)),
                 custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                 ..Default::default()
             },
@@ -43,44 +61,33 @@ fn create_tile(commands: &mut Commands, num: u64, position: Position) {
         });
 }
 
-#[derive(Component)]
-struct Background;
-
 fn create_board(commands: &mut Commands) {
-    commands
-        .spawn_bundle(SpriteBundle {
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::BEIGE,
+            custom_size: Some(Vec2::new(TILE_SIZE * 4.4, TILE_SIZE * 4.4)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+    for i in 0..SIDE_LENGTH * SIDE_LENGTH {
+        commands.spawn_bundle(SpriteBundle {
             sprite: Sprite {
-                color: Color::BEIGE,
-                custom_size: Some(Vec2::new(TILE_SIZE * 4.4, TILE_SIZE * 4.4)),
+                color: Color::GRAY,
+                custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                 ..Default::default()
             },
+            transform: Position::from_index(i).into(),
             ..Default::default()
-        })
-        .insert(Background);
-    for i in 0..SIDE_LENGTH as i32 {
-        for j in 0..SIDE_LENGTH as i32 {
-            commands
-                .spawn_bundle(SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::GRAY,
-                        custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
-                        ..Default::default()
-                    },
-                    transform: Position::new(i, j).into(),
-                    ..Default::default()
-                })
-                .insert(Background);
-        }
+        });
     }
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     create_board(&mut commands);
-    for i in 1..SIDE_LENGTH as i32 {
-        for j in 1..SIDE_LENGTH as i32 {
-            create_tile(&mut commands, 2, Position::new(i, j));
-        }
+    for (idx, num) in [(1, 2), (3, 4), (13, 8)] {
+        create_tile(&mut commands, num, Position::from_index(idx));
     }
 }
 
@@ -95,5 +102,75 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_event::<MoveEvent>()
+        .add_system(send_move_event)
+        .add_system(move_tiles_system)
         .run();
+}
+
+#[derive(PartialEq, Eq)]
+enum MoveEvent {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+fn send_move_event(keyboard: Res<Input<KeyCode>>, mut ev_move: EventWriter<MoveEvent>) {
+    if keyboard.just_pressed(KeyCode::Left) {
+        ev_move.send(MoveEvent::Left)
+    } else if keyboard.just_pressed(KeyCode::Right) {
+        ev_move.send(MoveEvent::Right)
+    } else if keyboard.just_pressed(KeyCode::Up) {
+        ev_move.send(MoveEvent::Up)
+    } else if keyboard.just_pressed(KeyCode::Down) {
+        ev_move.send(MoveEvent::Down)
+    }
+}
+
+fn move_tiles_system(
+    mut ev_move: EventReader<MoveEvent>,
+    mut query: Query<(&mut Transform, &mut Position), With<Tile>>,
+) {
+    for ev in ev_move.iter() {
+        let (dx, dy, rot) = match ev {
+            MoveEvent::Left => (-1, 0, 0),
+            MoveEvent::Right => (1, 0, 2),
+            MoveEvent::Up => (0, 1, 3),
+            MoveEvent::Down => (0, -1, 1),
+        };
+
+        let mut map = vec![0; SIDE_LENGTH * SIDE_LENGTH];
+        for (_, pos) in query.iter() {
+            map[pos.index()] = 1;
+        }
+        for _ in 0..rot {
+            rotate_map(&mut map);
+        }
+        for i in 0..SIDE_LENGTH {
+            let mut v = 0;
+            for j in 0..SIDE_LENGTH {
+                v += 1 - map[i * SIDE_LENGTH + j];
+                map[i * SIDE_LENGTH + j] += v - 1;
+            }
+        }
+        for _ in rot..4 {
+            rotate_map(&mut map);
+        }
+        for (mut trans, mut pos) in query.iter_mut() {
+            let idx = pos.index();
+            pos.x += dx * map[idx];
+            pos.y += dy * map[idx];
+            *trans = pos.clone().into();
+        }
+    }
+}
+
+const ROT: [usize; SIDE_LENGTH * SIDE_LENGTH] =
+    [3, 7, 11, 15, 11, 6, 10, 14, 14, 10, 10, 13, 15, 13, 14, 15];
+
+fn rotate_map(a: &mut [i32]) {
+    for (i, &j) in ROT.iter().enumerate() {
+        a.swap(i, j)
+    }
 }
